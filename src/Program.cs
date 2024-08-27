228f905a805833175fa5e0ef991c9cbb588c3c11
anisul-islam <anisul2010s@yahoo.co.uk>
2024-08-27 12:52:08 +0300

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FunctionChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start from here");
+
+        }
+    }
+}
